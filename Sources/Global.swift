66afb40b29ab4777ae234ab27d4a92ec6175d54f
Thomas Franceschini <tumasgiu@mac.com>
2016-10-05 15:49:23 +0200

    Implement CW_USEDEFAULT, LOWORD and HIWORD macros in Swift

--- Sources/Global.swift
+++ Sources/Global.swift
@@ -19,3 +19,31 @@ func findWindowForHandle(_ handle: HWND) -> Window? {
 
     return window
 }
+
+/// Equivalent to CW_USEDEFAULT
+var useDefault: Int32 {
+    let base = UInt32(0x80000000)
+    return unsafeBitCast(base, to: Int32.self)
+}
+
+func lowWord<T>(of param: T) -> UInt16 {
+    var param = param
+    var result: UInt16 = 0
+    withUnsafePointer(to: &param) {
+        $0.withMemoryRebound(to: UInt16.self, capacity: 2) {
+            result = $0.pointee
+        }
+    }
+    return result
+}
+
+func highWord<T>(of param: T) -> UInt16 {
+    var param = param
+    var result: UInt16 = 0
+    withUnsafePointer(to: &param) {
+        $0.withMemoryRebound(to: UInt16.self, capacity: 2) {
+            result = $0.advanced(by: 1).pointee
+        }
+    }
+    return result
+}
